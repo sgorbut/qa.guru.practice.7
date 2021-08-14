e03b64759cd42923996124abe6ca918f0be80214
sgorbut <sgorbut@gmail.com>
2021-08-14 13:56:39 +0300

    added pdf and zip files tests

--- src/test/java/guru/qa/PdfTest.java
+++ src/test/java/guru/qa/PdfTest.java
@@ -1,4 +1,25 @@
 package guru.qa;
 
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import static org.assertj.core.api.Assertions.assertThat;
+import static com.codeborne.pdftest.PDF.containsText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PdfTest {
+
+    @Test
+    void testForPdf() throws IOException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader(); // Чтение файла из ресурсов через classLoader
+        InputStream stream = classLoader.getResourceAsStream("PDF.pdf"); // Чтение тестового файла
+        PDF pdfFile = new PDF(stream);
+        assertThat(pdfFile.creator).contains("LibreOffice 6.4.7.2");
+    }
 }
--- src/test/java/guru/qa/ZipTest.java
+++ src/test/java/guru/qa/ZipTest.java
@@ -1,12 +1,31 @@
 package guru.qa;
 
+import com.codeborne.xlstest.XLS;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
 
 public class ZipTest {
     @Test
-    void TestForZip (){
+    void TestForZip () throws IOException, ZipException {
 
+        ZipFile zipFile = new ZipFile("src/test/resources/ZIP.zip");
+        zipFile.setPassword("00000");
+        zipFile.extractAll("src/test/resources/");
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader(); // Чтение файла из ресурсов через classLoader
+        InputStream stream = classLoader.getResourceAsStream("XLS.xls"); // Чтение тестового файла
+        XLS xlsFile = new XLS(stream);
+        Assertions.assertEquals("bbb222", xlsFile.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue());
     }
 }
